9771e3833a95f5fae5e7402fa2776a0b2e1f18aa
Developonz <zhenya2k04@gmail.com>
2024-03-11 16:17:12 +0800

    Complete

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(79, 55);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(210, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(34, 20);
+            label1.TabIndex = 1;
+            label1.Text = "коп";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(79, 101);
+            button1.Name = "button1";
+            button1.Size = new Size(165, 29);
+            button1.TabIndex = 2;
+            button1.Text = "преобразовать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(318, 206);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,23 @@ namespace ifTask
         public Form1()
         {
             InitializeComponent();
+            textBox1.Text = Properties.Settings.Default.textBoxKop.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int mon;
+            try
+            {
+                mon = int.Parse(textBox1.Text);
+            } catch (FormatException)
+            {
+                return;
+            }
+            Properties.Settings.Default.textBoxKop = mon;
+            Properties.Settings.Default.Save();
+
+            MessageBox.Show(Logic.solver(mon));
         }
     }
     public class Logic
